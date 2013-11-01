3889f9cdb8a47371c88a8f2b37a080242ef6e430
brianbianco <brian.bianco@gmail.com>
2013-11-01 15:55:41 -0400

    Adds the --sync option
    Adds the command line option --sync (-s).  By default overseer will now just
    show you the changes and only sync them if you specify the -s switch.

--- lib/overseer/client.rb
+++ lib/overseer/client.rb
@@ -9,7 +9,7 @@ module Overseer
       group_def = Overseer.get_definition(options[:group])
       autoscale_def = Overseer::Autoscale.new(options[:group],group_def)
       autoscale_def.show_differences
-      autoscale_def.sync
+      autoscale_def.sync if options[:sync]
     end
 
   end
