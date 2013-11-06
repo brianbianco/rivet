32135823329b12f389fff2aa163adcda3487017a
brianbianco <brian.bianco@gmail.com>
2013-11-06 16:43:40 -0500

    Overseer now quits with a message when given a group that doesn't exist.

--- lib/overseer/client.rb
+++ lib/overseer/client.rb
@@ -9,6 +9,9 @@ module Overseer
       Overseer::Utils.ensure_minimum_setup
 
       group_def = Overseer::Utils.get_definition(options[:group])
+
+      Overseer::Utils.die "The #{options[:group]} definition doesn't exist" unless group_def
+
       Overseer::Log.info("Checking #{options[:group]} autoscaling definition")
       autoscale_def = Overseer::Autoscale.new(options[:group],group_def)
       autoscale_def.show_differences
