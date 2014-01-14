c6c9d985800b8b124e97be51c9004af2750806fa
brianbianco <brian.bianco@gmail.com>
2014-01-14 15:05:43 -0500

    Adds the ConfigProxy class.
    This class is meant to make it easy to normalize config data without having to
    mutate the state of the config objects.  It calls the normalize method for a
    config option if it exists.

---
+++ lib/rivet/config_proxy.rb
@@ -0,0 +1,26 @@
+module Rivet
+  class ConfigProxy < BasicObject
+
+    def initialize(config)
+      @config = config
+    end
+
+    def send(m,*args)
+      if @config.respond_to?("normalize_#{m}".to_sym)
+        @config.send("normalize_#{m}".to_sym)
+      else
+        super
+      end
+    end
+
+    def method_missing(m, *args, &block)
+      if @config.respond_to?("normalize_#{m}".to_sym)
+        @config.send("normalize_#{m}".to_sym)
+      elsif @config.respond_to? m
+        @config.send(m, *args, &block)
+      else
+        super
+      end
+    end
+  end
+end
