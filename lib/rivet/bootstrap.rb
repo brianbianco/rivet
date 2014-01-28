b4564dcfb6a3d79da1aed6e6935d9db69e34f20c
brianbianco <brian.bianco@gmail.com>
2014-01-28 11:44:30 -0500

    Returns an empty string for userdata if no template is provided.

--- lib/rivet/bootstrap.rb
+++ lib/rivet/bootstrap.rb
@@ -13,8 +13,12 @@ module Rivet
     protected
 
     def generate_user_data
-      template = ERB.new(File.read(config.template))
-      template.result(config.instance_eval { binding })
+      if config.respond_to?(:template)
+        template = ERB.new(File.read(config.template))
+        template.result(config.instance_eval { binding })
+      else
+        ""
+      end
     end
   end
 end
