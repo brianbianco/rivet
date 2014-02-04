f3ce2c02f8c04d490eb18ef4b87e9da6219588e7
brianbianco <brian.bianco@gmail.com>
2014-02-04 13:22:15 -0500

    Moves the OpenState class into it's own file.

--- lib/rivet/config.rb
+++ lib/rivet/config.rb
@@ -1,45 +1,4 @@
 module Rivet
-  class OpenState
-    attr_reader :generated_attributes
-    attr_accessor :required_fields
-
-    def initialize
-      @generated_attributes = []
-    end
-
-    def install_get_or_set(name)
-      @generated_attributes << name
-      define_singleton_method(name) do |*args|
-        if args.size < 1
-          instance_variable_get("@#{name}")
-        else
-          instance_variable_set("@#{name}",args[0])
-        end
-      end
-    end
-
-    def validate
-      required_fields.each_pair do |method,default_value|
-        unless respond_to?(method)
-          if default_value.nil?
-            raise "Required field #{method} missing!"
-          else
-            send(method,default_value)
-          end
-        end
-      end
-    end
-
-    def method_missing(m,*args,&block)
-      if args.size < 1
-        super
-      else
-        install_get_or_set(m)
-        send(m,args[0])
-      end
-    end
-  end
-
   class Config < OpenState
     attr_reader :name
     attr_accessor :bootstrap,:required_fields
@@ -123,6 +82,5 @@ module Rivet
     def import(import_path)
       lambda { eval(File.read(import_path)) }.call
     end
-
   end
 end
