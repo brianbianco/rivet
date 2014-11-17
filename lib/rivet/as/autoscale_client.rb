d1db37acbdbdea39247e5b034d548b74cf2d51a9
brianbianco <brian.bianco@gmail.com>
2014-11-17 10:03:10 -0500

    Futher reorganization and renaming of classes in preperation for addition of EC2

--- lib/rivet/as/autoscale_client.rb
+++ lib/rivet/as/autoscale_client.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 module Rivet
-  class Client
+  class AutoscaleClient
     def run(options)
       AwsUtils.set_aws_credentials options.profile
       Rivet::Log.level options.log_level
