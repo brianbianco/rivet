2e60d3a295ef52d8de2c1e49e79ef77d787f2dbd
brianbianco <brian.bianco@gmail.com>
2014-11-18 15:05:03 -0500

    Reorganizes specs

--- spec/as/autoscale_config_spec.rb
+++ spec/as/autoscale_config_spec.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
-require_relative './spec_setup'
-require_relative './shared_examples/a_config'
+require_relative '../spec_setup'
+require_relative '../shared_examples/a_config'
 
 include SpecHelpers
 
