0422caec84ef3def8334bec36163ce8e9f7d768b
brianbianco <brian.bianco@gmail.com>
2014-11-25 15:12:27 -0500

    Adds example configuration files for ec2 servers

---
+++ example/ec2/example.rb
@@ -0,0 +1,8 @@
+import path('defaults.rb')
+count 1
+template path('user_data.erb')
+availability_zone 'a'
+tags [ { key: 'Name', value: 'example server' } ]
+
+bootstrap.template path('user_data.erb')
+bootstrap.packages ['ruby1.9.1', 'ruby1.9.1-dev', 'build-essential']
